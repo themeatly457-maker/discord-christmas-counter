0fbd5cb002a0289a53ece3013b0573026238743b
themeatly457-maker <themeatly457@gmail.com>
2025-12-11 08:53:02 -0500

    Update web.py

--- web.py
+++ web.py
@@ -1,5 +1,16 @@
 from flask import Flask
 import os
+import threading
+
+# ======== EJECUTAR BOT ========
+
+def run_bot():
+    os.system("python bot.py")
+
+t = threading.Thread(target=run_bot)
+t.start()
+
+# ======== SERVIDOR PARA RENDER ========
 
 app = Flask(__name__)
 
