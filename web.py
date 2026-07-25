5adfb6888c63f563fa368c416421890f6142fb75
themeatly457-maker <themeatly457@gmail.com>
2026-07-24 19:15:30 -0500

    Update web.py

--- web.py
+++ web.py
@@ -1,24 +1,22 @@
 from flask import Flask
-import os
 import threading
-
-# ======== EJECUTAR BOT ========
+import os
 
 def run_bot():
     os.system("python bot.py")
 
-t = threading.Thread(target=run_bot)
-t.start()
-
-# ======== SERVIDOR PARA RENDER ========
+threading.Thread(target=run_bot).start()
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Bot Discord funcionando."
+    return "Bot funcionando."
 
-port = int(os.environ.get("PORT", 10000))
+@app.route("/ping")
+def ping():
+    return "OK"
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=port)
+port = int(os.environ.get("PORT",10000))
+
+app.run(host="0.0.0.0",port=port)
